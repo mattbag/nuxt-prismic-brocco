196946e3c66721c3869b1b968573912b620d5eae
matt <matt@atlasagency.com.au>
2020-05-12 15:02:40 +1000

    custom scrolling

--- components/global/Nav.vue
+++ components/global/Nav.vue
@@ -18,7 +18,7 @@
         <li>
           <button
             id="hamburger"
-            class="bg-white-tOFF text-pink p-1 off js-slide-close focus:out"
+            class="bg-white-tOFF text-pink p-1 off js-slide-close focus:outline-none"
           >
             <Burger />
             <!-- menu -->
@@ -27,32 +27,25 @@
       </ul>
     </nav>
     <div class="menudrop bg-black">
-      <nav class="menudrop__nav uppercase">
+      <nav class="menudrop__nav">
         <ul>
           <li>
-            <nuxt-link
-              class="js-menu-off font-bold font-sans text-stroke-w hover:text-white text-r6"
-              :to="{ path: '/',hash:'#home'}"
-            >
-              home</nuxt-link>
+            <button
+              @click="clickNav('/','#home')"
+              class="js-menu-off font-bold font-sans text-stroke-w hover:text-white text-r6 uppercase focus:outline-none"
+            >home</button>
           </li>
           <li>
-            <nuxt-link
-              :to="{ path: '/',hash:'#work'}"
-              class="js-menu-off font-bold font-sans text-stroke-w hover:text-white text-r6"
-            >Work</nuxt-link>
+            <button
+              @click="clickNav('/','#work')"
+              class="js-menu-off font-bold font-sans text-stroke-w hover:text-white text-r6 uppercase focus:outline-none"
+            >work</button>
           </li>
-          <!-- <li>
-            <a
-              href="#contact"
-              class="js-menu-off text-stroke-w hover:text-white text-r6"
-            >Contact</a>
-          </li> -->
           <li>
-            <nuxt-link
-              :to="{ path: '',hash:'#contact'}"
-              class="js-menu-off text-stroke-w hover:text-white text-r6"
-            >Contact</nuxt-link>
+            <button
+              @click="clickNav('','#contact')"
+              class="js-menu-off font-sans text-stroke-w hover:text-white text-r6 uppercase focus:outline-none"
+            >contact</button>
           </li>
         </ul>
       </nav>
@@ -66,6 +59,40 @@ export default {
   components: { Burger },
   mounted() {
     window.initNav();
+  },
+  methods: {
+    scroller(offset) {
+      window.scroll({
+        top: offset,
+        left: 0,
+        behavior: "smooth"
+      });
+    },
+    clickNav(route, hash) {
+      let $item = document.querySelector(hash);
+      if ($item) {
+        setTimeout(_ => {
+          this.scroller($item.offsetTop);
+        }, 500);
+      } else {
+        if (route) {
+          this.$router.push(route, function() {
+            setTimeout(() => {
+              // console.log("====");
+              // console.log("oncomplete", document.querySelector(hash));
+              // console.log("====");
+              let $item = document.querySelector(hash);
+              // this.scroller($item.offsetTop);
+              window.scroll({
+                top: $item.offsetTop,
+                left: 0,
+                behavior: "smooth"
+              });
+            }, 1000);
+          });
+        }
+      }
+    }
   }
 };
 </script>
